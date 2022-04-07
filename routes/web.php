9e6758e3f8672a312365c9cf4c6f4f69c6a80910
Amine-Mhani <aminemhani11@gmail.com>
2022-04-07 11:09:25 +0000

    S

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,15 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
